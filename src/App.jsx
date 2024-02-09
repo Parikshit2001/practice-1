66ec6fb8f45a733ef3f2933e8fd9719249a026dc
Parikshit Gupta <parikshitgupta@Parikshits-MacBook-Air.local>
2024-02-09 23:49:51 +0530

    Hitting backend endpoint deployed to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+
+
+    const apiUrl = 'https://practice-2-psi.vercel.app/';
+
+    axios.get(apiUrl)
+    .then((response) => {
+      console.log("Successful GET request: ", response)
+      setData(prev => ({...prev, getData: response.data}))
+    })
+    .catch((error) => console.error("Error while making GET request", error))
+
+    axios.post(apiUrl)
+    .then((response) => {
+      console.log("Successful POST request: ", response)
+      setData(prev => ({...prev, postData: response.data}))
+    })
+    .catch((error) => console.error("Error while making POST request", error))
+
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>APP</h1>
+      <p>GET DATA: {data.getData}</p>
+      <p>POST DATA: {data.postData}</p>
+    </div>
   )
 }
 
